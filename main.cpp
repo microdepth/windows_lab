dc5d3e898bb514163d803c1247bca67f461702af
ps <psdaboss2@gmail.com>
2025-07-01 22:03:55 -0400

    ClientToScreenRect

--- main.cpp
+++ main.cpp
@@ -16,19 +16,31 @@ const WCHAR WINDOW_NAME[] = L"window name";
 
 class DPIScale {
     static float scale;
-
 public:
     static void Initialize(HWND hwnd) {
         float dpi = GetDpiForWindow(hwnd);
         scale = dpi / 96.0f;
     }
-
     template <typename T>
     static D2D1_POINT_2F PixelsToDips(T x, T y) {
         return D2D1::Point2F(static_cast<float>(x) / scale, static_cast<float>(y) / scale);
     }
 };
 float DPIScale::scale = 1.0f;
+
+BOOL ClientToScreenRect(HWND hwnd, LPRECT rect) {
+    GetClientRect(hwnd, rect);
+
+    POINT pt1 = {rect->left, rect->top};
+    POINT pt2 = {rect->right, rect->bottom};
+
+    ClientToScreen(hwnd, &pt1);
+    ClientToScreen(hwnd, &pt2);
+
+    SetRect(rect, pt1.x, pt1.y, pt2.x, pt2.y);
+
+    return TRUE;
+}
 
 class Scene : public GraphicsScene {
     CComPtr<ID2D1SolidColorBrush> m_pGreen;
